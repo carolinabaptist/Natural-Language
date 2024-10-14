afcad1289151c3bc7b3b21a0f1f606bbb24de212
Ana Carolina Baptista <119694053+carolinabaptist@users.noreply.github.com>
2024-10-14 20:19:00 +0100

    Create ex-1.py

---
+++ Exercises/ex-1.py
@@ -0,0 +1,6 @@
+#Import nltk and download the ‘stopwords’ and ‘punkt’ packages
+# from https://www.machinelearningplus.com/nlp/nlp-exercises/
+
+import nltk
+nltk.download('punkt') #tokenizer
+nltk.download('stopwords')
